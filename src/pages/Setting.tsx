3f13276141b19fe1ebfb41e4354a871177e5bace
Hamin Lee <whereanna7@gmail.com>
2025-04-24 00:34:35 +0900

    [ADD] add contact information on setting page

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -2,7 +2,16 @@ function Setting() {
   return (
     <div className="w-full my-[0] mx-auto px-[30px] sm:px-[190px] bg-[#F5F5F5] min-h-[calc(100vh-131px)]">
       <br />
-      <p className="text-center sm:text-left">Setting page is coming soon!</p>
+      <p className="text-center sm:text-left">
+        For more information about this app, please contact to{" "}
+        <a
+          href="https://www.linkedin.com/in/hamin-lee-40317a199/"
+          target="_blank"
+          className="font-medium hover:text-[#2362EA] duration-150 ease-in"
+        >
+          Hamin Lee
+        </a>
+      </p>
     </div>
   );
 }
